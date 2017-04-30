50245252eafc43fa63633f1a002f361ae6970887
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2017-04-30 10:50:49 +0200

    [helpers] Update git fetch helper with libreelec-8.0

--- update-from-upstreams.sh
+++ update-from-upstreams.sh
@@ -4,6 +4,6 @@ echo "Current branch is: $CURRENT_BRANCH"
 git fetch --tags upstream-ro && \
 git checkout master && \
 git merge upstream-ro/master && \
-git checkout libreelec-7.0 && \
-git merge upstream-ro/libreelec-7.0 && \
+git checkout libreelec-8.0 && \
+git merge upstream-ro/libreelec-8.0 && \
 git checkout $CURRENT_BRANCH
